3e3529a19070954a70f36aa332de819861ddb78e
Maxime Guillemin <guimaxali@gmail.com>
2025-11-23 16:51:40 +0100

    fix(web): disable logs capture for SSE snapshots

--- src/application/web/sse.rs
+++ src/application/web/sse.rs
@@ -327,6 +327,7 @@ pub(super) async fn sse_stream(
                         resolve_socket_processes: false,
                         with_network_traffic: exposure.network_traffic(),
                         with_updates: false,
+                        with_logs: false,
                     },
                     exposure,
                     #[cfg(feature = "config")]
