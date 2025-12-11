8e89888ee20cd06ef0f8daee4c45e1826dcd2c89
Maxime Guillemin <guimaxali@gmail.com>
2025-12-11 11:33:20 +0100

    `feat(logs): register and re-export FreeBSD syslog backend module`

--- src/infrastructure/logs/mod.rs
+++ src/infrastructure/logs/mod.rs
@@ -1,6 +1,13 @@
 #[cfg(all(feature = "journald", target_os = "linux"))]
 pub mod linux;
 
+#[cfg(target_os = "freebsd")]
+pub mod freebsd;
+
 #[cfg(all(feature = "journald", target_os = "linux"))]
 #[allow(unused_imports)]
 pub use linux::JournaldBackend;
+
+#[cfg(target_os = "freebsd")]
+#[allow(unused_imports)]
+pub use freebsd::FreebsdSyslogBackend;
